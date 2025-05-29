8fbebb31a6784105457ddbca0b50550e5712e8f0
Soyoung Park <soyoung008@naver.com>
2025-05-29 20:58:03 +0900

    Chore: svgr 설치 및 설정

---
+++ src/assets/icons/index.js
@@ -0,0 +1,3 @@
+import PlayIcon from '@/assets/icons/playIcon.svg?react';
+
+export { PlayIcon };
--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr({ include: '**/*.svg?react' })],
   resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
 });
