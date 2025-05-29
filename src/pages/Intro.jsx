86b69dfaef17fec02843493299a41e8707c36044
Soyoung Park <soyoung008@naver.com>
2025-05-30 00:16:15 +0900

    Design: Intro UI 구현

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,5 +1,13 @@
+import Button from '@/components/commons/Button';
+
 const Intro = () => {
-  return <>Intro</>;
+  return (
+    <main className="h-screen bg-[url(assets/images/intro.png)] bg-cover bg-center">
+      <div className="fixed top-2/3 right-1/2 translate-x-1/2">
+        <Button buttonText="START" textSize="36px" padding="20px" />
+      </div>
+    </main>
+  );
 };
 
 export default Intro;
