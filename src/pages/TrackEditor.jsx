44e457a85c500418808e308c6a67046a3c729665
= <bosoco@naver.com>
2025-05-30 18:29:51 +0900

    Design: TrackEditor 컴포넌트 구현(공통 UI 레이아웃 및 인터랙션)

--- src/pages/TrackEditor.jsx
+++ src/pages/TrackEditor.jsx
@@ -1,5 +1,31 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+import Panel from '@/components/Panel';
+import PanelButton from '@/components/PanelButton';
+
 const TrackEditor = () => {
-  return <>TrackEditor</>;
+  const [activeButton, setActiveButton] = useState('');
+  const [trackMode, setTrackMode] = useState('');
+
+  const handleButtonClick = (buttonType) => {
+    setActiveButton((prev) => (prev === buttonType ? '' : buttonType));
+    setTrackMode(buttonType);
+  };
+
+  return (
+    <div className="fixed bottom-0 w-full transition-all duration-500 overflow-visible ">
+      <PanelButton activeButton={activeButton} onButtonClick={handleButtonClick} />
+      <div className={`w-full transition-all duration-500 ${activeButton ? 'h-[138px]' : 'h-0'}`}>
+        <Panel isOpen={!!activeButton} trackMode={trackMode} />
+      </div>
+    </div>
+  );
+};
+
+PanelButton.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default TrackEditor;
