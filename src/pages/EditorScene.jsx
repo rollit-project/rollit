82319be33352a3be71ac922b5f65e47e233bc86c
lee hyewon <leehyewon959@gmail.com>
2025-05-30 21:45:15 +0900

    feat: R3F 환경 구축, 렌더링 기본 구조 구현

---
+++ src/pages/EditorScene.jsx
@@ -0,0 +1,14 @@
+import { OrbitControls, RoundedBox, Sky } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+
+import EditorCanvas from '../components/EditorCanvas';
+
+const EditorScene = () => {
+  return (
+    <>
+      <EditorCanvas />
+    </>
+  );
+};
+
+export default EditorScene;
