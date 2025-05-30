9356050b3494906984d83064f6c8290ea8041f9a
lee hyewon <leehyewon959@gmail.com>
2025-05-30 15:16:34 +0900

    Refactor: 시멘틱 마크업 적용을 위한 section, ul/li 구조 변경

--- src/components/MeasurementPanel.jsx
+++ src/components/MeasurementPanel.jsx
@@ -1,4 +1,3 @@
-// components/MeasurementPanel.jsx
 const measurementData = [
   { name: '속도', measurement: '85.2 km/h' },
   { name: '중력가속도', measurement: '6.3 m/s²' },
@@ -8,16 +7,18 @@ const measurementData = [
 
 const Measurement = () => {
   return (
-    <div className="flex justify-end p-5">
+    <section className="flex justify-end p-5">
       <div className="h-[190px] w-[260px] rounded-xl bg-black/40 px-6 py-6 text-white">
-        {measurementData.map(({ name, measurement }) => (
-          <div key={name} className="mb-4 flex justify-between">
-            <div className="text-right font-medium">{name}:</div>
-            <div>{measurement}</div>
-          </div>
-        ))}
+        <ul>
+          {measurementData.map(({ name, measurement }) => (
+            <li key={name} className="mb-4 flex justify-between">
+              <span className="text-right font-medium">{name}:</span>
+              <span>{measurement}</span>
+            </li>
+          ))}
+        </ul>
       </div>
-    </div>
+    </section>
   );
 };
 
