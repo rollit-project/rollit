4fe2d5789b36963d26d3ba552f8e29c8bcf75a61
= <bosoco@naver.com>
2025-06-01 00:19:21 +0900

    Refactor: TrackEditor의 불필요한 로직을 Panel 컴포넌트로 이동하여 역할 분리

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,47 +1,53 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-import carousel from '@/assets/images/carousel.png';
-import chair from '@/assets/images/chair.png';
-import coster from '@/assets/images/coster.png';
-import cream from '@/assets/images/cream.png';
-import racer from '@/assets/images/racer.png';
-import smile from '@/assets/images/smile.png';
-import stall from '@/assets/images/stall.png';
-import steel from '@/assets/images/steel.png';
-import stone from '@/assets/images/stone.png';
-import vine from '@/assets/images/vine.png';
-import windmill from '@/assets/images/windmill.png';
 import PanelList from '@/components/PanelList';
+import { imageList } from '@/utils/imageMapping';
 
-const Panel = ({ isOpen, trackMode }) => {
-  const costerImages = [coster];
-  const trackImages = [steel, stone, vine, smile];
-  const itemImages = [carousel, chair, cream, racer, windmill, stall, coster];
-  const imageSets = {
-    coster: costerImages,
-    track: trackImages,
-    item: itemImages,
+import PanelActionButton from './PanelActionButton';
+import PanelButton from './PanelButton';
+
+const Panel = () => {
+  const [trackMode, setTrackMode] = useState('');
+  const [isPanelOpen, setIsPanelOpen] = useState(false);
+  const handlePanelToggle = (buttonType) => {
+    if (trackMode !== buttonType) {
+      setTrackMode(buttonType);
+      setIsPanelOpen(true);
+    } else {
+      setIsPanelOpen(false);
+      setTrackMode('');
+    }
   };
-  const imageList = (track) => imageSets[track] || [];
 
   return (
-    <div
-      className={`h-[138px] w-full pr-5 pl-5 ${
-        isOpen ? 'bg-[rgba(0,0,0,0.3)]' : 'bg-[#FFFFFF]'
-      } z-40 flex items-center`}
-    >
-      <PanelList imageList={imageList(trackMode)} />
+    <div className="fixed bottom-0 flex w-full flex-col items-center transition-all duration-500">
+      <div
+        className={`flex w-full items-center justify-between transition-transform duration-500${
+          isPanelOpen ? 'translate-y-[-200px]' : 'translate-y-0'
+        }`}
+      >
+        <PanelButton trackMode={trackMode} onButtonClick={handlePanelToggle} />
+        <PanelActionButton />
+      </div>
+      <div
+        className={`w-full overflow-hidden bg-[rgba(0,0,0,0.3)] transition-all duration-500 ${
+          isPanelOpen ? 'h-[150px] pt-5' : 'h-0'
+        } flex justify-start`}
+      >
+        <PanelList imageList={imageList(trackMode)} />
+      </div>
     </div>
   );
 };
 
-Panel.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
+PanelButton.propTypes = {
   trackMode: PropTypes.string,
+  onButtonClick: PropTypes.func.isRequired,
 };
 
 PanelList.propTypes = {
-  imageList: PropTypes.arrayOf(PropTypes.string),
+  imageList: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Panel;
--- src/pages/TrackEditor.jsx
+++ src/pages/TrackEditor.jsx
@@ -1,31 +1,11 @@
-import PropTypes from 'prop-types';
-import { useState } from 'react';
-
 import Panel from '@/components/Panel';
-import PanelButton from '@/components/PanelButton';
 
 const TrackEditor = () => {
-  const [activeButton, setActiveButton] = useState('');
-  const [trackMode, setTrackMode] = useState('');
-
-  const handleButtonClick = (buttonType) => {
-    setActiveButton((prev) => (prev === buttonType ? '' : buttonType));
-    setTrackMode(buttonType);
-  };
-
   return (
-    <div className="fixed bottom-0 w-full overflow-visible transition-all duration-500">
-      <PanelButton activeButton={activeButton} onButtonClick={handleButtonClick} />
-      <div className={`w-full transition-all duration-500 ${activeButton ? 'h-[138px]' : 'h-0'}`}>
-        <Panel isOpen={!!activeButton} trackMode={trackMode} />
-      </div>
+    <div className="h-screen bg-[url(assets/images/background.png)] bg-cover bg-center">
+      <Panel />
     </div>
   );
-};
-
-PanelButton.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
-  onClick: PropTypes.func.isRequired,
 };
 
 export default TrackEditor;
