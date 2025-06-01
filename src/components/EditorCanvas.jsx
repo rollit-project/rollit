d1889b768950df10d62c7186ac519d2590f20d30
Soyoung Park <soyoung008@naver.com>
2025-06-01 15:25:05 +0900

    Feat: WASD 키 입력에 따른 카메라 이동 구현

--- src/components/EditorCanvas.jsx
+++ src/components/EditorCanvas.jsx
@@ -1,10 +1,16 @@
 import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import PropTypes from 'prop-types';
+import { useRef } from 'react';
+
+import MoveControls from '@/components/MoveControls';
 
 const EditorCanvas = ({ cameraRotationSpeed, cameraMoveSpeed }) => {
+  const orbitControlsRef = useRef();
+
   return (
     <Canvas camera={{ position: [0, 0, 2], fov: 75 }}>
+      <MoveControls moveSpeed={cameraMoveSpeed} orbitControlsRef={orbitControlsRef} />
       <ambientLight intensity={0.5} />
       <directionalLight position={[5, 5, 5]} />
       <mesh position={[0, 0, 0]}>
@@ -12,6 +18,7 @@ const EditorCanvas = ({ cameraRotationSpeed, cameraMoveSpeed }) => {
         <meshStandardMaterial color="skyblue" />
       </mesh>
       <OrbitControls
+        ref={orbitControlsRef}
         mouseButtons={{
           LEFT: null,
           MIDDLE: 0,
