0436645fbf7e15643f7a5aa6ddd287bc3bbc9551
= <bosoco@naver.com>
2025-06-01 20:41:36 +0900

    Refactor: 아이콘 네이밍을 직관적으로 변경 (back → undo)

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,6 +1,6 @@
-import RollerCoasterBackIcon from '@/assets/icons/roller-coaster-back-icon.svg?react';
 import RollerCoasterIcon from '@/assets/icons/roller-coaster-icon.svg?react';
 import RollerCoasterPlayIcon from '@/assets/icons/roller-coaster-play-icon.svg?react';
 import RollerCoasterResetIcon from '@/assets/icons/roller-coaster-reset-icon.svg?react';
+import RollerCoasterUndoIcon from '@/assets/icons/roller-coaster-undo-icon.svg?react';
 
-export { RollerCoasterBackIcon, RollerCoasterIcon, RollerCoasterPlayIcon, RollerCoasterResetIcon };
+export { RollerCoasterIcon, RollerCoasterPlayIcon, RollerCoasterResetIcon, RollerCoasterUndoIcon };
