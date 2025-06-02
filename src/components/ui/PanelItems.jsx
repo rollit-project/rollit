451aaaf78a6accc175c679489e0ac6c7e4469ca7
Soyoung Park <soyoung008@naver.com>
2025-06-02 23:25:48 +0900

    Feat: 패널 클릭 시 선택한 아이템을 상태에 저장하여 전달

--- src/components/ui/PanelItems.jsx
+++ src/components/ui/PanelItems.jsx
@@ -1,15 +1,16 @@
 import PropTypes from 'prop-types';
 
-const PanelItems = ({ imageList }) => {
+const PanelItems = ({ imageList, handleSelectItem }) => {
   return (
     <div className="scrollbar-hidden flex h-4/5 gap-[30px] pl-5 [&::-webkit-scrollbar]:hidden">
       {imageList.map((image) => (
-        <img
-          key={image.name}
-          className="h-full w-full flex-1 object-contain hover:scale-110"
-          src={image.src}
-          alt={image.name}
-        />
+        <button key={image.name} type="button" onClick={() => handleSelectItem(image.name)}>
+          <img
+            className="h-full w-full flex-1 object-contain hover:scale-110"
+            src={image.src}
+            alt={image.name}
+          />
+        </button>
       ))}
     </div>
   );
@@ -17,6 +18,7 @@ const PanelItems = ({ imageList }) => {
 
 PanelItems.propTypes = {
   imageList: PropTypes.arrayOf(PropTypes.string).isRequired,
+  handleSelectItem: PropTypes.func.isRequired,
 };
 
 export default PanelItems;
