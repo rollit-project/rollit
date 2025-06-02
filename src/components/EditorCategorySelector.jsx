0182a3655a101ea5f41ecef9551f33a38974f08d
= <bosoco@naver.com>
2025-06-02 14:00:58 +0900

    Refactor: 컴포넌트 내부 로직 최적화 및 불필요한 속성 정리

--- src/components/EditorCategorySelector.jsx
+++ src/components/EditorCategorySelector.jsx
@@ -1,28 +1,19 @@
 import PropTypes from 'prop-types';
 
 const EditorCategorySelector = ({ activePanelType, onButtonClick }) => {
-  const PANEL_INFO = [
-    { type: 'coster', name: 'centerButton' },
-    { type: 'track', name: 'trackButton' },
-    { type: 'item', name: 'itemButton' },
-  ];
+  const PANEL_INFO = ['coster', 'track', 'item'];
 
   return (
     <div className="relative h-[50px] w-[480px] rounded-tr-[10px] text-center transition-all duration-500">
       <div className="flex h-full items-center justify-around">
-        {PANEL_INFO.map(({ type, name }) => (
+        {PANEL_INFO.map((type) => (
           <button
             type="button"
-            name={name}
             key={type}
-            className={`h-full flex-1 cursor-pointer px-4 font-bold text-white transition-all duration-300 ${
-              activePanelType === name
-                ? 'bg-[rgba(0,0,0,0.3)]'
-                : 'border-2 border-white bg-[rgba(255,255,255,0.3)]'
-            }`}
+            className={`h-full flex-1 cursor-pointer px-4 font-bold text-white transition-all duration-300 ${activePanelType === type ? 'bg-[rgba(0,0,0,0.3)]' : 'border-2 border-white bg-white'} ${type === 'item' ? 'rounded-tr-[10px]' : ''}`}
             onClick={() => onButtonClick(type)}
           >
-            {name}
+            {type}
           </button>
         ))}
       </div>
