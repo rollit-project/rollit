155fc41283090309dca8440f2d98d2f245e6b9b9
lee hyewon <leehyewon959@gmail.com>
2025-06-03 16:28:47 +0900

    Design: 롤러코스터 기둥 본체 구현

---
+++ src/components/ui/CoasterPillar.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import * as THREE from 'three';
+
+const CoasterPillar = ({ positionX = 0, pillarHeight = 5 }) => {
+  const centerY = pillarHeight / 2;
+
+  const material = new THREE.MeshStandardMaterial({
+    color: '#888888',
+    metalness: 0.6,
+    roughness: 0.4,
+  });
+
+  return (
+    <group position={[positionX, 0, 0]}>
+      <mesh position={[0, centerY, 0]} material={material} castShadow>
+        <cylinderGeometry args={[0.2, 0.2, pillarHeight, 16]} />
+      </mesh>
+    </group>
+  );
+};
+
+CoasterPillar.propTypes = {
+  positionX: PropTypes.number,
+  pillarHeight: PropTypes.number,
+};
+
+export default CoasterPillar;
