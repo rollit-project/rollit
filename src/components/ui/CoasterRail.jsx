a977c70bc67c2d1173e4c8bc51426bb0ba834d61
lee hyewon <leehyewon959@gmail.com>
2025-06-03 19:20:33 +0900

    Design: 단일 레일 조각 반복 로직 구현

--- src/components/ui/CoasterRail.jsx
+++ src/components/ui/CoasterRail.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import * as THREE from 'three';
 
 const material = new THREE.MeshStandardMaterial({
@@ -6,28 +7,44 @@ const material = new THREE.MeshStandardMaterial({
   roughness: 0.4,
 });
 
-const CoasterRail = () => {
+const CoasterRail = ({ segmentCount = 5, segmentGap = 1.5, origin = [0, 0, 0] }) => {
   const railRadius = 0.2;
   const railSpacing = 3;
-  const sleeperWidth = 3;
+  const sleeperWidth = railSpacing;
   const sleeperThickness = 0.3;
   const sleeperHeight = 0.1;
 
-  return (
-    <group>
-      <mesh position={[-railSpacing / 2, 0, 0]} material={material} rotation={[Math.PI / 2, 0, 0]}>
-        <cylinderGeometry args={[railRadius, railRadius, 1.5, 16]} />
-      </mesh>
+  const segments = Array.from({ length: segmentCount }, (_, index) => {
+    const segmentOffsetZ = index * segmentGap;
 
-      <mesh position={[railSpacing / 2, 0, 0]} material={material} rotation={[Math.PI / 2, 0, 0]}>
-        <cylinderGeometry args={[railRadius, railRadius, 1.5, 16]} />
-      </mesh>
+    return (
+      <group key={index} position={[0, 0, segmentOffsetZ]}>
+        <mesh
+          position={[-railSpacing / 2, 0, 0]}
+          material={material}
+          rotation={[Math.PI / 2, 0, 0]}
+        >
+          <cylinderGeometry args={[railRadius, railRadius, segmentGap, 16]} />
+        </mesh>
 
-      <mesh position={[0, 0, 0]} material={material}>
-        <boxGeometry args={[sleeperWidth, sleeperHeight, sleeperThickness]} />
-      </mesh>
-    </group>
-  );
+        <mesh position={[railSpacing / 2, 0, 0]} material={material} rotation={[Math.PI / 2, 0, 0]}>
+          <cylinderGeometry args={[railRadius, railRadius, segmentGap, 16]} />
+        </mesh>
+
+        <mesh position={[0, 0, 0]} material={material}>
+          <boxGeometry args={[sleeperWidth, sleeperHeight, sleeperThickness]} />
+        </mesh>
+      </group>
+    );
+  });
+
+  return <group position={origin}>{segments}</group>;
+};
+
+CoasterRail.propTypes = {
+  segmentCount: PropTypes.number,
+  segmentGap: PropTypes.number,
+  origin: PropTypes.arrayOf(PropTypes.number),
 };
 
 export default CoasterRail;
