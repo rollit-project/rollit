a0cd6cb7705815bd5c772b9a18f058466a792fad
lee hyewon <leehyewon959@gmail.com>
2025-06-04 14:49:10 +0900

    Feat: 바닥에 나무 GLB 모델 배치 및 scale 조정

--- src/components/scene/Ground.jsx
+++ src/components/scene/Ground.jsx
@@ -1,9 +1,16 @@
+import { useGLTF } from '@react-three/drei';
+
 const Ground = () => {
+  const { scene } = useGLTF('/objects/tree.glb');
+
   return (
-    <mesh position={[0, -1, 0]} receiveShadow>
-      <boxGeometry args={[100, 1, 100]} />
-      <meshStandardMaterial color="#b4e07b" />
-    </mesh>
+    <group>
+      <mesh position={[0, -1, 0]} receiveShadow>
+        <boxGeometry args={[100, 1, 100]} />
+        <meshStandardMaterial color="#b4e07b" />
+      </mesh>
+      <primitive object={scene} position={[0, -0.5, 0]} scale={[0.15, 0.15, 0.15]} />
+    </group>
   );
 };
 
