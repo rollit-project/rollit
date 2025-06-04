ef7f40e3084928fe006c755569820e4c49b5aeaf
lee hyewon <leehyewon959@gmail.com>
2025-06-04 15:02:24 +0900

    Feat: 나무 모델 다중 배치 구현

--- src/components/scene/Ground.jsx
+++ src/components/scene/Ground.jsx
@@ -3,13 +3,29 @@ import { useGLTF } from '@react-three/drei';
 const Ground = () => {
   const { scene } = useGLTF('/objects/tree.glb');
 
+  const treePositions = [
+    [-10, -0.5, -3],
+    [20, -0.5, -20],
+    [-20, -0.5, 10],
+    [-2, -0.5, 30],
+    [10, -0.5, 30],
+  ];
+
   return (
     <group>
       <mesh position={[0, -1, 0]} receiveShadow>
         <boxGeometry args={[100, 1, 100]} />
         <meshStandardMaterial color="#b4e07b" />
       </mesh>
-      <primitive object={scene} position={[0, -0.5, 0]} scale={[0.15, 0.15, 0.15]} />
+
+      {treePositions.map((position) => (
+        <primitive
+          key={position.join('-')}
+          object={scene.clone()}
+          position={position}
+          scale={[0.02, 0.02, 0.02]}
+        />
+      ))}
     </group>
   );
 };
