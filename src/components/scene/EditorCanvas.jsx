df2a55634de9741178052974eb2cfcb05da50efe
= <bosoco@naver.com>
2025-06-08 17:25:41 +0900

    Design: 롤러코스터 입구 고정 배치

--- src/components/scene/EditorCanvas.jsx
+++ src/components/scene/EditorCanvas.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, useGLTF } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import PropTypes from 'prop-types';
 import { useRef, useState } from 'react';
@@ -12,7 +12,8 @@ import MoveControls from '@/components/scene/MoveControls';
 const EditorCanvas = ({ cameraRotationSpeed, cameraMoveSpeed, selectedItem, handleSelectItem }) => {
   const orbitControlsRef = useRef();
   const [placedItems, setPlacedItems] = useState([]);
-
+  const { scene: coaster } = useGLTF('/objects/coaster.glb');
+  const { scene: railStraight } = useGLTF('/objects/rail-straight.glb');
   const handlePlaceItems = (item) => {
     setPlacedItems((prev) => [...prev, item]);
   };
@@ -20,7 +21,11 @@ const EditorCanvas = ({ cameraRotationSpeed, cameraMoveSpeed, selectedItem, hand
   return (
     <Canvas shadows camera={{ position: [0, 5, 10], fov: 75 }}>
       <color attach="background" args={['#b0eaff']} />
-      <MouseFollower selectedItem={selectedItem} />
+      <MouseFollower
+        selectedItem={selectedItem}
+        handlePlaceItems={handlePlaceItems}
+        handleSelectItem={handleSelectItem}
+      />
       <MoveControls moveSpeed={cameraMoveSpeed} orbitControlsRef={orbitControlsRef} />
       <OrbitControls
         ref={orbitControlsRef}
@@ -35,18 +40,13 @@ const EditorCanvas = ({ cameraRotationSpeed, cameraMoveSpeed, selectedItem, hand
       />
       <ambientLight intensity={0.4} />
       <DirectionalLight />
-      <mesh position={[0, 0, 0]} castShadow>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="skyblue" />
-      </mesh>
-      <MouseFollower
-        selectedItem={selectedItem}
-        handlePlaceItems={handlePlaceItems}
-        handleSelectItem={handleSelectItem}
-      />
       {placedItems.map((item) => (
         <ItemModel key={item.id} selectedItem={item.name} position={item.position} />
       ))}
+      <primitive object={coaster.clone()} position={[0, 0, 0]} />
+      <primitive object={railStraight.clone()} position={[0, 0, 0]} />
+      <primitive object={railStraight.clone()} position={[0, 0, 6]} />
+      <primitive object={railStraight.clone()} position={[0, 0, 12]} />
       <Ground />
       <gridHelper args={[10, 10, 'red', 'white']} position={[0, -0.5, 0]} />
     </Canvas>
