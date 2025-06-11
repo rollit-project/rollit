51c06823fa35abba77e9d0d4cb817b95f22a0979
Soyoung Park <soyoung008@naver.com>
2025-06-12 01:58:54 +0900

    Feat: 레일 포인트 중복을 제거하여 부드러운 곡선을 생성하는 유틸 함수 구현 및 적용

---
+++ src/utils/generateSmoothCurvePoints.js
@@ -0,0 +1,19 @@
+export function generateSmoothCurvePoints(placedRails) {
+  if (!placedRails || placedRails.length === 0) {
+    return [];
+  }
+
+  const smoothPoints = [];
+
+  for (let i = 0; i < placedRails.length; i += 1) {
+    const currentPoints = placedRails[i].points;
+
+    if (i === 0) {
+      smoothPoints.push(...currentPoints);
+    } else {
+      smoothPoints.push(...currentPoints.slice(1));
+    }
+  }
+
+  return smoothPoints;
+}
