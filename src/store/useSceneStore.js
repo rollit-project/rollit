8823a9ca68009177020a66538bc2898755a46a69
= <bosoco@naver.com>
2025-06-12 19:53:14 +0900

    Feat: 레일 상태 undo & reset 기능 추가

--- src/store/useSceneStore.js
+++ src/store/useSceneStore.js
@@ -1,14 +1,40 @@
 import { create } from 'zustand';
 
+import { INITIAL_RAILS } from '@/constants/initialRails';
+
 export const useSceneStore = create((set) => ({
   selectedItem: null,
   selectedRail: null,
   coasterPath: null,
   placedItems: [],
   placedRails: [],
+  railHistory: [],
   setSelectedItem: (item) => set({ selectedItem: item }),
   setSelectedRail: (rail) => set({ selectedRail: rail }),
   setCoasterPath: (path) => set({ coasterPath: path }),
   setPlacedItems: (item) => set({ placedItems: item }),
-  setPlacedRails: (rail) => set({ placedRails: rail }),
+  setPlacedRails: (rails) =>
+    set((state) => ({
+      placedRails: rails,
+      railHistory: [...state.railHistory, state.placedRails],
+    })),
+  undoRail: () =>
+    set((state) => {
+      if (state.railHistory.length === 0) {
+        return {};
+      }
+
+      const newHistory = [...state.railHistory];
+      const previous = newHistory.pop();
+
+      return {
+        placedRails: previous,
+        railHistory: newHistory,
+      };
+    }),
+  resetRails: () =>
+    set(() => ({
+      placedRails: [...INITIAL_RAILS],
+      railHistory: [],
+    })),
 }));
