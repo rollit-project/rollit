f5512d5c8ea434cebbed25d622755e1aea660b14
Soyoung Park <soyoung008@naver.com>
2025-06-14 03:07:30 +0900

    Feat: 레일 포인트 정의 및 이미지,모델 매핑 추가

--- src/constants/railPointTemplates.js
+++ src/constants/railPointTemplates.js
@@ -2,17 +2,39 @@ import * as THREE from 'three';
 
 export const RAIL_POINT_TEMPLATES = {
   straight: [new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, -6)],
-  up: [
+  upCombined: [
     new THREE.Vector3(0, 0, 0),
     new THREE.Vector3(0, 0.6, -2),
     new THREE.Vector3(0, 3, -4.8),
     new THREE.Vector3(0, 3.4, -6),
   ],
-  down: [
+  downCombined: [
     new THREE.Vector3(0, 0, 0),
     new THREE.Vector3(0, -0.6, -2),
     new THREE.Vector3(0, -3, -4.8),
     new THREE.Vector3(0, -3.4, -6),
+  ],
+  upStart: [
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(0, 0.4, -1.6),
+    new THREE.Vector3(0, 0.6, -2.2),
+  ],
+  upMiddle: [new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 2.4, -2.6)],
+  upEnd: [
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(0, 0.4, -0.6),
+    new THREE.Vector3(0, 0.4, -1.7),
+  ],
+  downStart: [
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(0, 0, -0.6),
+    new THREE.Vector3(0, -0.4, -1.7),
+  ],
+  downMiddle: [new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, -2.4, -2.6)],
+  downEnd: [
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(0, -0.2, -0.6),
+    new THREE.Vector3(0, -0.6, -2.2),
   ],
   curveLeft: [
     new THREE.Vector3(0, 0, 0),
