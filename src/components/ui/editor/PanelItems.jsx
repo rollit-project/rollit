b9ae7e3958a8d9631244b5b5b1b9ae9e24280058
Soyoung Park <soyoung008@naver.com>
2025-06-14 03:11:34 +0900

    Feat: 레일 패널 아이템에 툴팁 설명 표시

--- src/components/ui/editor/PanelItems.jsx
+++ src/components/ui/editor/PanelItems.jsx
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types';
 
+import { RAIL_TOOLTIP_MAP } from '@/constants/railTooltipMap';
+
 const PanelItems = ({ imageList, onClick }) => {
+  const getTooltipText = (name) => RAIL_TOOLTIP_MAP[name] ?? '설명 없음';
+
   return (
-    <div className="scrollbar-hidden flex h-4/5 gap-[30px] pl-5 [&::-webkit-scrollbar]:hidden">
+    <div className="scrollbar-hidden flex h-[120px] gap-[30px] overflow-x-auto pr-5 pl-5 [&::-webkit-scrollbar]:hidden">
       {imageList.map((image) => (
         <button
-          key={image.name}
           type="button"
-          onClick={() => {
-            onClick(image.name);
-          }}
+          onClick={() => onClick(image.name)}
+          title={getTooltipText(image.name)}
+          className="flex h-[120px] w-[200px] flex-shrink-0"
         >
           <img
             className="h-full w-full flex-1 object-contain hover:scale-110"
