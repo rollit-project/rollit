4e268c6c33d4aedb94282d830be86187031f9803
= <bosoco@naver.com>
2025-06-15 01:27:26 +0900

    Feat: 사운드 설정 및 레일 사운드 경로 정의

---
+++ src/constants/sound.js
@@ -0,0 +1,16 @@
+export const SOUND_CONFIG = {
+  BGM: {
+    DEFAULT_VOLUME: 0.01,
+    MAX_VOLUME: 1.0,
+    MIN_VOLUME: 0.0,
+  },
+  SFX: {
+    RAIL_INSTALL_VOLUME: 1.0,
+  },
+};
+
+export const RAIL_SOUNDS = {
+  straight: '/sounds/rail-straight.mp3',
+  up: '/sounds/rail-up.mp3',
+  down: '/sounds/rail-down.mp3',
+};
