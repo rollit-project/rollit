e4f173b040e0e20ea62d6b7e5e5ebcd863588b5b
Soyoung Park <soyoung008@naver.com>
2025-06-15 00:07:32 +0900

    Feat: 루프 연결 z축 허용 범위 추가 - 방향 정렬은 이후 보완 예정

--- src/utils/isRailConnected.js
+++ src/utils/isRailConnected.js
@@ -12,5 +12,12 @@ export const isRailConnected = () => {
   const start = new THREE.Vector3(...placedRails[0].position);
   const end = new THREE.Vector3(...placedRails.at(-1).endPoint);
 
-  return start.distanceTo(end) < 0.01;
+  const MIN_CONNECTION_GAP_Z = 0.5;
+  const MAX_CONNECTION_GAP_Z = 6;
+
+  const zGap = start.z - end.z;
+
+  const isConnected = zGap >= -MIN_CONNECTION_GAP_Z && zGap <= MAX_CONNECTION_GAP_Z;
+
+  return isConnected;
 };
