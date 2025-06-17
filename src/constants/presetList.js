37e43abfd6b45e14c09b56d2d9b8782ce27161e2
Soyoung Park <soyoung008@naver.com>
2025-06-18 01:39:20 +0900

    Feat: 프리셋 구성 확장

--- src/constants/presetList.js
+++ src/constants/presetList.js
@@ -1,12 +1,17 @@
 export const PRESET_LIST = [
   {
+    name: 'easy',
+    src: '/images/preset-easy.png',
+    json: '/presets/easy.json',
+  },
+  {
+    name: 'normal',
+    src: '/images/preset-normal.png',
+    json: '/presets/normal.json',
+  },
+  {
     name: 'extreme',
     src: '/images/preset-extreme.png',
     json: '/presets/extreme.json',
-  },
-  {
-    name: 'zigzag',
-    src: '/images/preset-zigzag.png',
-    json: '/presets/zigzag.json',
   },
 ];
