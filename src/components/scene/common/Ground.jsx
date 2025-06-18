e159c809365e1f709c17b226892d2bac8ecb1838
soyoung <102568726+qwp0@users.noreply.github.com>
2025-06-18 21:44:10 +0900

    Design: 전체 UI 및 초기 환경 세팅 개선 (#87)
    - 초기 카메라 위치 재설정으로 시야 개선
    - 바닥 영역 확장 및 트리 위치 조정
    - 슬라이더 컴포넌트 명칭 수정 및 라벨 텍스트 명확화
    - 에디터 패널의 탭 명칭을 레일 / 추천 코스 / 소품으로 재정의

--- src/components/scene/common/Ground.jsx
+++ src/components/scene/common/Ground.jsx
@@ -4,16 +4,16 @@ const Ground = () => {
   const { scene } = useGLTF('/objects/tree.glb');
 
   const treePositions = [
-    [30, -0.5, -30],
-    [-30, -0.5, -30],
+    [30, -0.5, -130],
+    [-30, -0.5, -130],
     [-30, -0.5, 30],
     [30, -0.5, 30],
   ];
 
   return (
     <group>
-      <mesh position={[0, -1, 0]} receiveShadow>
-        <boxGeometry args={[100, 1, 100]} />
+      <mesh position={[0, -1, -50]} receiveShadow>
+        <boxGeometry args={[100, 1, 200]} />
         <meshStandardMaterial color="#b4e07b" />
       </mesh>
 
