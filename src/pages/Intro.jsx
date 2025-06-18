1f434f9fd84a0b080e1374e5aede0b5820a3eeda
= <bosoco@naver.com>
2025-06-18 15:17:33 +0900

    Refactor: Intro 진입 시 유저 클릭을 통해 BGM 수동 재생 처리

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,10 +1,20 @@
 import { Link } from 'react-router-dom';
 
+import { useAudio } from '@/hooks/useAudio';
+
 const Intro = () => {
+  const { playBgm } = useAudio();
+
+  const handleStart = () => {
+    localStorage.setItem('bgm_enabled', 'true');
+    playBgm('/sounds/intro.mp3');
+  };
+
   return (
     <main className="h-screen bg-[url(/images/intro.png)] bg-cover bg-center">
       <Link
         to="/editor"
+        onClick={handleStart}
         className="btn-primary fixed top-2/3 right-1/2 translate-x-1/2 p-[20px] text-[36px]"
       >
         START
