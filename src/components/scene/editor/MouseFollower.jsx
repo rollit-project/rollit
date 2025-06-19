c70f0cebaa1b537caf8329425fcbfd17410aaabb
= <bosoco@naver.com>
2025-06-19 11:54:36 +0900

    Refactor: 아이템 설치 로직 분리 후 적용

--- src/components/scene/editor/MouseFollower.jsx
+++ src/components/scene/editor/MouseFollower.jsx
@@ -1,15 +1,11 @@
 import { useThree } from '@react-three/fiber';
 import { useCallback, useEffect, useRef, useState } from 'react';
-import toast from 'react-hot-toast';
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { v4 as uuidv4 } from 'uuid';
 
 import ItemModel from '@/components/scene/common/ItemModel';
 import { useItemStore } from '@/store/useItemStore';
 import { useRailStore } from '@/store/useRailStore';
-import { isObjectCollidingWithSceneObjects } from '@/utils/isObjectCollidingWithSceneObjects';
-import { getModelPathByName } from '@/utils/sceneAssetUtils';
+import { validateAndPlaceItem } from '@/utils/validateAndPlaceItem';
 
 const MouseFollower = () => {
   const { camera, gl } = useThree();
@@ -69,51 +65,16 @@ const MouseFollower = () => {
         return;
       }
 
-      try {
-        const loader = new GLTFLoader();
-        const gltf = await loader.loadAsync(getModelPathByName(selectedItem));
-        const mesh = gltf.scene.clone();
-
-        mesh.position.copy(intersect);
-        mesh.rotation.set(0, rotationY, 0);
-
-        const isItemCollision = await isObjectCollidingWithSceneObjects({
-          targetObject: mesh,
-          sceneObjects: placedItems,
-          getModelPath: (item) => getModelPathByName(item.name),
-          getPosition: (item) => item.position,
-          getRotation: (item) => [0, item.rotationY || 0, 0],
-        });
-
-        const isRailCollision = await isObjectCollidingWithSceneObjects({
-          targetObject: mesh,
-          sceneObjects: placedRails,
-          getModelPath: (rail) => rail.modelPath,
-          getPosition: (rail) => rail.position,
-          getRotation: (rail) => rail.rotation || [0, 0, 0],
-        });
-
-        if (isItemCollision || isRailCollision) {
-          toast.error('충돌로 인해 설치할 수 없습니다.');
-
-          return;
-        }
-
-        setPlacedItems([
-          ...placedItems,
-          {
-            name: selectedItem,
-            position: intersect.clone(),
-            id: uuidv4(),
-            rotationY,
-          },
-        ]);
-        setSelectedItem(null);
-        setRotationY(0);
-      } catch (err) {
-        console.error('❌ 모델 로딩 실패:', err);
-        toast.error('모델 로딩 실패로 설치할 수 없습니다.');
-      }
+      await validateAndPlaceItem({
+        selectedItem,
+        position: intersect,
+        rotationY,
+        placedItems,
+        placedRails,
+        setPlacedItems,
+        setSelectedItem,
+        setRotationY,
+      });
     };
 
     const dom = gl.domElement;
